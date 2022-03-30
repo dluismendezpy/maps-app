cd7787fd32e521fdd6e1ea77150007a8ba9252af
Luis Mendez <dluismendezdj@gmail.com>
2022-03-30 16:44:54 -0400

    Use createStackNavigator

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Form from "./src/Form";
+import MapScreen from "./src/MapScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+const App = createStackNavigator({
+  Form: { screen: Form },
+  MapScreen: { screen: MapScreen },
 });
+
+export default createAppContainer(App);
